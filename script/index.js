4a795c396cf37d5f5763e47cb9dcbb7ab5603444
yolandamatiwane <mbalentle.matiwane21@gmail.com>
2024-06-05 11:23:49 +0200

    fixed code

--- script/index.js
+++ script/index.js
@@ -4,15 +4,62 @@ let out_2 = document.querySelector('#g')
 let out_3 = document.querySelector('#b')
 let btn = document.querySelector('button')
 
-function hexToRgb(hex){
-    let r = parseInt(hex.substring(0,2),16);
-    let g = parseInt(hex.substring(2,4),16);
-    let b = parseInt(hex.substring(4,6),16)
-    return {r,g,b};
+let string = ''
+
+function toRgb(r){
+    m = r
+    switch (true) {
+        case m<10:
+            return m
+            break;
+    
+        case m == 'A':
+            return 10
+            break;
+    
+        case m == 'B':
+            return 11
+            break;
+    
+        case m == 'C':
+            return 12
+            break;
+    
+        case m == 'D':
+            return 13
+            break;
+    
+        case m == 'E':
+            return 14
+            break;
+        case m == 'F':
+            return 15
+            break;
+    
+        default:
+            break;
+    }
 }
 
 btn.addEventListener('click',()=>{
-    let hexColor = inp.value
-    out_1 = hexToRgb(hexColor)
+    string = inp.value
+    let [redW,redR] = string.substring(1,3).split('')
+    let [greenW,greenR] = string.substring(3,5).split('')
+    let [blueW,blueR] = string.substring(5,7).split('')
+    function result_r(){
+        let red = eval(`(${toRgb(redW)}*${16})+${toRgb(redR)}`)
+        return red;
+    }
+    function result_g(){
+        let green = eval(`(${toRgb(greenW)}*${16}) + ${toRgb(greenR)}`)
+        return green;
+    }
+    function result_b(){
+        let blue = eval(`${toRgb(blueW)} *${16}+${toRgb(blueR)}`)
+        return blue;
+    }
+    out_1.innerText = result_r()
+    out_2.innerText = result_g()
+    out_3.innerText = result_b()
 
 })
